c000b5f6772fdc9a315837d38e97a64ced02012c
Andrei Kucherov <andrei@kucherov.net>
2019-01-30 08:12:29 +1100

    Files structure has been changed

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import os
 from binance.client import Client
 from binance.websockets import BinanceSocketManager
-
-def process_message(msg):
-    print(msg)
+from trader import Trader
 
 api_key = os.environ['BINANCE_API_KEY']
 api_secret = os.environ['BINANCE_API_SECRET']
 
 client = Client(api_key, api_secret)
 bm = BinanceSocketManager(client)
-conn_key = bm.start_kline_socket('BTCUSDT', process_message, interval=Client.KLINE_INTERVAL_1MINUTE)
+tr = Trader()
+conn_key = bm.start_kline_socket('BTCUSDT', tr.monitor, interval=Client.KLINE_INTERVAL_1MINUTE)
 
 bm.start()
-
---
+++ trader.py
@@ -0,0 +1,7 @@
+class Trader:
+
+    def __init__(self):
+        self.history = []
+
+    def monitor(self, msg):
+        print(msg)
