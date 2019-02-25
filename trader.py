d5c66373fc2b711f27ec0f4e9b1da8ed1249fb94
Andrei Kucherov <andrei@authenticateit.com>
2019-02-25 16:00:46 +1100

    Sell signal has been changed

--- trader.py
+++ trader.py
@@ -308,13 +308,11 @@ class Trader:
     def sellSignal(self, asset):
         data = asset['data']
         if len(data) > 2:
-            m1 = data[-2]['macd']
-            m2 = data[-3]['macd']
             r1 = data[-2]['rsi6']
             r2 = data[-3]['rsi6']
             (p3, p4) = asset['params'][-2:]
-            if not (m1 is None or m2 is None):
-                return r2 > p3 and r1 < p3 and (m2 - m1) > p4
+            if not (r1 is None or r2 is None):
+                return r2 > p3 and r1 < p4 
             else:
                 return False
         else:
