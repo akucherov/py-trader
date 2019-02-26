c4a66c5ea54dfbf82f0c7b500ff12d488a8f922b
Andrei Kucherov <andrei@authenticateit.com>
2019-02-26 13:36:21 +1100

    test is fixed

--- test.py
+++ test.py
@@ -6,7 +6,6 @@ api_key = os.environ['BINANCE_API_KEY']
 api_secret = os.environ['BINANCE_API_SECRET']
 
 quote = sys.argv[1] if len(sys.argv) > 1 else "USDT"
-assetsConfig = sys.argv[2] if len(sys.argv) > 2 else "assets.txt"
-testConfig = sys.argv[3] if len(sys.argv) > 3 else "assets.test.txt"
+assetsConfig = sys.argv[2] if len(sys.argv) > 2 else "assets.test.txt"
 
-Trader(api_key, api_secret, quote, assetsConfig, True).tune('8 weeks ago', testConfig)
+Trader(api_key, api_secret, quote, assetsConfig, True).tune('8 weeks ago', assetsConfig)
