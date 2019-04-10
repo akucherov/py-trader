ce76bd9e86f9778f0499333d24705610b60acb40
Andrei Kucherov <andrei@authenticateit.com>
2019-04-10 17:13:54 +1000

    Review support lines

--- offline-test.py
+++ offline-test.py
@@ -1,7 +1,7 @@
 from OfflineTestTrader import OfflineTestTrader
 from SupportLinesStrategy import SupportLinesStrategy
 
-strategy = SupportLinesStrategy(n=36, k1=1, k3=1)
+strategy = SupportLinesStrategy(n=36, k1=1, k3=1, size=144)
 trader = OfflineTestTrader("./data/2019-1-1-BTCUSDT-5m.csv", strategy, 1000, 100)
 
 trader.run()
